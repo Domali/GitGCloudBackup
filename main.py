e83c188db2c8dc3ba0bf3994a7c5b57be970aee6
Sean Renaud <domali1875@gmail.com>
2018-06-06 22:40:23 -0500

    Test commit to check server

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import os
+from google.cloud import storage
 def main():
-    print("test")
-    
+    dir_to_check = r''
+    json_key = r'D:\Seans\CloudKeys\MyFirstProject-e2cc2e0f8671.json'
+    client = storage.Client.from_service_account_json(json_key)
+    bucket = client.get_bucket('dom-git-bucket')
+    blob = bucket.blob('testfile.txt')
+    blob.upload_from_filename(r'C:\Users\domal.DESKTOP-08KELV1\Desktop\testoftestyay.txt')
+
 if __name__ =="__main__":
     main()
