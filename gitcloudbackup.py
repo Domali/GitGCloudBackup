08c1f20387ea920b89e2cce699c98a9f3ca55679
Sean Renaud <domali1875@gmail.com>
2018-06-09 18:29:18 -0500

    Change over to a config.json add stack system messages,update gitignore

--- gitcloudbackup.py
+++ gitcloudbackup.py
@@ -3,33 +3,82 @@ import zlib
 import zipfile
 import re
 import tempfile
+import json
+import requests
+import platform
 from google.cloud import storage
 from absl import app
 from absl import flags
 from absl import logging
 
 FLAGS = flags.FLAGS
-flags.DEFINE_string('bucket',None,'Bucket to use on Google Cloud Storage')
-flags.DEFINE_string('keyfile',None,'Google Cloud keyfile')
-flags.DEFINE_string('filedir',None,'Directory to zip when changes are found')
-flags.DEFINE_string('zipname',None,'Name of zip file(without extensions ie just repos not repos.zip')
+flags.DEFINE_string('config',None,"Location of a JSON formatted configuration file")
+flags.DEFINE_bool('debug',False,"Flag to debug application")
 
 def main(argv):
-  logging.set_verbosity(logging.INFO)
-  zip_filename = FLAGS.zipname
-  zip_directory = FLAGS.filedir
-  client = storage.Client.from_service_account_json(FLAGS.keyfile)
-  bucket = client.get_bucket(FLAGS.bucket)
-  if AreLocalFilesNewer(bucket, zip_directory):
-    UploadZippedDirectory(bucket,zip_directory,zip_filename)
+  if FLAGS.debug:
+    logging.set_verbosity(logging.DEBUG)
+  else:
+    logging.set_verbosity(logging.INFO)
+  config = LoadConfigFile(FLAGS.config)
+  zfname = config['zipname']
+  zdir = config['filedir']
+  keyf = config['keyfile']
+  bname = config['bucket']
+  saurl = config['saurl']
+  client = storage.Client.from_service_account_json(keyf)
+  bucket = client.get_bucket(bname)
+  if AreLocalFilesNewer(bucket, zdir):
+    UploadZippedDirectory(bucket,zdir,zfname)
+    SendStackMessage(saurl, "Files uploaded to cloud starge")
     logging.info("Files uploaded to cloud storage")
   else:
     logging.info("No local file changes")
 
+def SendStackMessage(saurl, text):
+  """ This method takes a Stack authenticated url and sends the text to it.
+  Parameters
+  ----------
+  saurl : str
+    The authenticated URL
+  text : str
+    The text to send
+  """
+  PAYLOAD = {"text": text,}
+  res = requests.post(saurl, json=PAYLOAD)
+
+def LoadConfigFile(jsonf=None):
+  """ This method takes a file name and loads a JSON config from it.  
+  If the value of the file is None then it tries to load a config.json
+  file located in the same path as the program.
+
+  Paramters:
+  ---------
+  jsonf : str
+    string representing the filename to load
+  Return
+  ------
+  dict
+    A dictionary with key pairs from the JSON file
+    """
+  if jsonf == None:
+    delimiter = "/" if platform.system() != "Windows" else "\\"
+    file_path = "/".join(
+      os.path.realpath(__file__).split(delimiter)[0:-1] +
+        [ "config.json", ])
+  else:
+    file_path = jsonf
+  logging.debug('JSON Config File: ' + file_path)
+  with open(file_path, "r") as file:
+    config = json.load(file)
+  logging.debug(config)
+  return config
+
 def UploadZippedDirectory(bucket,zipdir,zipname):
   """ This method zips a directory and uploads it to a
   google cloud storage bucket
-  Parameters
+
+  Parameters:
   ----------
   bucket : google.cloud.storage.bucket
     The bucket to upload to
